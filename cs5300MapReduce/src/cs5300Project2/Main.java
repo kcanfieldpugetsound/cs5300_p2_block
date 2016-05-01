a31589a16cc6f6c654f861741a0701616b788471
Gus Donnelly <gmd68@cornell.edu>
2016-05-01 19:17:19 -0400

    fixes step

--- cs5300MapReduce/src/cs5300Project2/Main.java
+++ cs5300MapReduce/src/cs5300Project2/Main.java
@@ -15,10 +15,13 @@ public class Main {
 	private static final String usage = "USAGE FOR: Blocked PageRank | MapReduce\n"
 		+ "\nto preprocess data:"
 			+ "\n\tpre edge_filepath block_filepath output_directory"
+		
 		+ "\nto run program:"
 			+ "\n\trun input_filename output_directory num_runs"
+		
 		+ "\nto run another iteration (once already started):"
 			+ "\n\tstep output_directory"
+		
 		+ "\nfor help:"
 			+ "\n\thelp";
 	
@@ -27,12 +30,18 @@ public class Main {
 			+ "\n\tproduces the initial input to the MapReduce algorithm "
 			+ "\n\tusing the given edge file and block file, and outputs "
 			+ "\n\tthe result to output_directory/input.txt"
+			
 		+ "\nrun usage: run input_filename output_directory num_runs "
 			+ "\n\truns MapReduce num_runs times using input_filename for "
 			+ "\n\tthe input for the first iteration, and stores intermediate "
 			+ "\n\tdata in output_directory; note that the input file must not "
 			+ "\n\tbe in output_directory, as output_directory is cleared "
-			+ "\n\tbefore each run";
+			+ "\n\tbefore each run"
+			
+		+ "\nstep usage: step output_directory [num_steps]"
+			+ "\n\tsearches output_directory for the most recent output, and "
+			+ "\n\truns one (or num_steps if provided) iterations using this "
+			+ "\n\tmost recent output as input";
 	
 	public static void main (String... elephants) {
 		try {
@@ -44,7 +53,11 @@ public class Main {
 				run(elephants[1], elephants[2], elephants[3]);
 				break;
 			case "step":
-				step(elephants[1]);
+				if (elephants.length > 2) {
+					step(elephants[1], elephants[2]);
+				} else {
+					step(elephants[1], String.valueOf(1));
+				}
 				break;
 			case "usage":
 				System.out.println(usage);
@@ -58,6 +71,7 @@ public class Main {
 			}
 		} catch (Exception e) {
 			System.out.println("Invalid arguments or internal error.");
+			e.printStackTrace();
 			System.out.println(usage);
 		}
 	}
@@ -90,29 +104,41 @@ public class Main {
 		}
 	}
 	
-	public static void step (String outputDirectory) throws Exception {
+	public static void step (String outputDirectory, String numSteps) throws Exception {
 		Configuration config = new Configuration();
 		Path outputDir = new Path(outputDirectory);
 		FileSystem fs = outputDir.getFileSystem(config);
 		Path inputFile = null;
+		
+		int NUM_STEPS = Integer.parseInt(numSteps);
+		
+		//I don't know how to get children of the output directory, so 
+		//I'll just have to guess and check... sigh... but it works...
+		//I'm guessing you won't be running more than 100 iterations...
 		int max_dir = 100;
 		int i;
 		for (i = max_dir; i >= 0; i--) {
 			Path p = new Path(outputDirectory + "/" + i);
-			if (fs.exists(inputFile)) {
+			if (fs.exists(p)) {
 				inputFile = p;
 				break;
 			}
 		}
 		
 		if (inputFile == null) {
-			System.out.println("run `run' before using step; run `usage' or "
-				+ "`help' for more information");
+			System.out.println("`run' must be called before using step; "
+				+ "`usage' or `help' for more information");
 		} else {
-			System.out.println("Running `step' with input " + i 
-				+ " and output " + (i + 1));
-			Path outputFile = new Path(outputDirectory + "/" + (i + 1));
-			runPageRank(inputFile, outputFile);
+			int iterationNumber = i + 1;
+			Path outputFile;
+			for (int j = 0; j < NUM_STEPS; j++) {
+				outputFile = new Path(outputDirectory + "/" + iterationNumber);
+				runPageRank(inputFile, outputFile);
+				iterationNumber++;
+				inputFile = outputFile;
+			}
+			System.out.println("ran `step' " + NUM_STEPS + " time(s) with input " + i 
+				+ " and output " + iterationNumber);
 		}
 	}
 	
