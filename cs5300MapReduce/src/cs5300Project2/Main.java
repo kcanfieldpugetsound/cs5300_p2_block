f9d4c3562b7f3b103a9271c42fef844ea979c6ca
Gus Donnelly <gmd68@cornell.edu>
2016-05-01 18:51:59 -0400

    adds step functionality

--- cs5300MapReduce/src/cs5300Project2/Main.java
+++ cs5300MapReduce/src/cs5300Project2/Main.java
@@ -1,6 +1,7 @@
 package cs5300Project2;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -16,6 +17,8 @@ public class Main {
 			+ "\n\tpre edge_filepath block_filepath output_directory"
 		+ "\nto run program:"
 			+ "\n\trun input_filename output_directory num_runs"
+		+ "\nto run another iteration (once already started):"
+			+ "\n\tstep output_directory"
 		+ "\nfor help:"
 			+ "\n\thelp";
 	
@@ -39,6 +42,12 @@ public class Main {
 				break;
 			case "run":
 				run(elephants[1], elephants[2], elephants[3]);
+				break;
+			case "step":
+				step(elephants[1]);
+				break;
+			case "usage":
+				System.out.println(usage);
 				break;
 			case "help":
 				System.out.println(help);
@@ -73,13 +82,37 @@ public class Main {
 		
 		//run the actual mapreduce job
 		for (int i = 0; i < NUM_RUNS; i++) {
-			System.out.println("running pagerank iteration " + i);
 			Path output = new Path(outputDir, String.valueOf(i));
 			
 			runPageRank(input, output);
 			
 			input = output;
-			System.out.println("\tcompleted pagerank iteration " + i);
+		}
+	}
+	
+	public static void step (String outputDirectory) throws Exception {
+		Configuration config = new Configuration();
+		Path outputDir = new Path(outputDirectory);
+		FileSystem fs = outputDir.getFileSystem(config);
+		Path inputFile = null;
+		int max_dir = 100;
+		int i;
+		for (i = max_dir; i >= 0; i--) {
+			Path p = new Path(outputDirectory + "/" + i);
+			if (fs.exists(inputFile)) {
+				inputFile = p;
+				break;
+			}
+		}
+		
+		if (inputFile == null) {
+			System.out.println("run `run' before using step; run `usage' or "
+				+ "`help' for more information");
+		} else {
+			System.out.println("Running `step' with input " + i 
+				+ " and output " + (i + 1));
+			Path outputFile = new Path(outputDirectory + "/" + (i + 1));
+			runPageRank(inputFile, outputFile);
 		}
 	}
 	
